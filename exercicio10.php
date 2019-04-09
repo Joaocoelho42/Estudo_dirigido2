c780097effab8af96d8047254fa7baee85e996f2
joão Vitor Coêlho <48602999+Joaocoelho42@users.noreply.github.com>
2019-04-09 16:01:46 -0300

    Create exercicio10.php

---
+++ exercicio10.php
@@ -0,0 +1,31 @@
+<?php
+
+print "Digite o valor de A: ";
+$a=(float)fgets(STDIN);
+
+if ($a==0){
+    exit;
+}
+
+print "Digite o valor de B: ";
+$b=(float)fgets(STDIN);
+
+print "Digite o valor de C: ";
+$c=(float)fgets(STDIN);
+
+    $delta=(pow($b,2)-(4*$a*$c));
+
+    if ($delta>0){
+        print "Há duas raízes reais\n";
+    }
+    elseif ($delta<0){
+        print "Não há raízes reais\n";
+        exit;
+    }
+    elseif ($delta==0){
+        print "Só há uma raíz real\n";
+    }
+    $x1=((-($b)+sqrt($delta))/(2*$a));
+    $x2=((-($b)-sqrt($delta))/(2*$a));
+    print "X1 = $x1\n";
+    print "X2 = $x2\n";
